d177ea603418ded7b4b34b2456af7b0ba3df868c
timmy <timmy56887@icloud.com>
2024-08-13 22:26:01 +0800

    test cd

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     output: 'export', // 配置 Next.js 輸出靜態網站
     trailingSlash: true, // 確保所有路徑都以 `/` 結尾
     reactStrictMode: true,
+    basePath: '/resume',
   };
   
   export default nextConfig;
