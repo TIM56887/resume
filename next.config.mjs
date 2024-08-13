33bc1ce0655e37be193c9800b051d129bdb6f93c
timmy <timmy56887@icloud.com>
2024-08-13 22:14:51 +0800

    test cd

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export', // 配置 Next.js 輸出靜態網站
+    trailingSlash: true, // 確保所有路徑都以 `/` 結尾
+    reactStrictMode: true,
+  };
+  
+  export default nextConfig;
+  
